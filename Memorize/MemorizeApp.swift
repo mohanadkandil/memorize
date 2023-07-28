3c8269673a9fefd416af0d527d043d61eac587ea
mkandil <mkandil@synapse-analytics.io>
2023-07-28 20:16:34 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mohanned Kandil on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
